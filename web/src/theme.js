5de36108d957a48695a9b195da7e6aad8e5e7cb3
Cs <kuokangmin@Cs-MacBook-Pro.local>
2024-09-06 21:54:44 +0800

    add Town Pass Figma

--- web/src/theme.js
+++ web/src/theme.js
@@ -2,17 +2,64 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
+    mode: 'light',
     primary: {
-      light: '#4dabf5',
-      main: '#2196f3',
-      dark: '#1769aa',
-      contrastText: '#fff',
+      main: '#5ab4c5',
+      light: '#93d4df',
+      dark: '#356c77',
+      contrastText: 'rgba(255,255,255,0.87)',
     },
     secondary: {
-      light: '#f73378',
-      main: '#f50057',
-      dark: '#ab003c',
-      contrastText: '#fff',
+      main: '#f5ba4b',
+      contrastText: 'rgba(255,255,255,0.87)',
+      dark: '#ad7b2b',
+      light: '#f4d69e',
+    },
+    error: {
+      main: '#d45251',
+      light: '#e29494',
+    },
+    warning: {
+      main: '#fd853a',
+      light: '#f4b992',
+      contrastText: 'rgba(255,255,255,0.87)',
+    },
+    background: {
+      paper: '#ffffff',
+    },
+    success: {
+      main: '#76a732',
+    },
+    text: {
+      primary: '#30383d',
+      secondary: '#475259',
+      disabled: '#475259',
+      hint: '#30383d',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: 36,
+      lineHeight: 1.33,
+    },
+    h2: {
+      fontSize: 24,
+      lineHeight: 1.33,
+    },
+    h3: {
+      fontSize: 16,
+      lineHeight: 1.37,
+    },
+    caption: {
+      fontSize: 12,
+      lineHeight: 1.5,
+    },
+    body1: {
+      fontSize: 14,
+      lineHeight: 1.43,
+    },
+    body2: {
+      fontSize: 14,
     },
   },
 });
