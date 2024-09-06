2a32d364baa3d74433721124c406cc63c918324d
Cs <kuokangmin@Cs-MacBook-Pro.local>
2024-09-06 21:54:44 +0800

    add Town Pass Figma

--- web/src/theme.js
+++ web/src/theme.js
@@ -3,31 +3,65 @@ const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            light: "#4dabf5",
-            main: "#2196f3",
-            dark: "#1769aa",
-            contrastText: "#fff",
-        },
-        secondary: {
-            light: "#f73378",
-            main: "#f50057",
-            dark: "#ab003c",
-            contrastText: "#fff",
-        },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#5ab4c5',
+      light: '#93d4df',
+      dark: '#356c77',
+      contrastText: 'rgba(255,255,255,0.87)',
     },
-    select: {
-        itemHeight: ITEM_HEIGHT,
-        itemPaddingTop: ITEM_PADDING_TOP,
-        MenuProps: {
-            PaperProps: {
-                style: {
-                    maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP * 2,
-                    width: 250,
-                },
-            },
-        },
+    secondary: {
+      main: '#f5ba4b',
+      contrastText: 'rgba(255,255,255,0.87)',
+      dark: '#ad7b2b',
+      light: '#f4d69e',
+    },
+    error: {
+      main: '#d45251',
+      light: '#e29494',
+    },
+    warning: {
+      main: '#fd853a',
+      light: '#f4b992',
+      contrastText: 'rgba(255,255,255,0.87)',
+    },
+    background: {
+      paper: '#ffffff',
+    },
+    success: {
+      main: '#76a732',
+    },
+    text: {
+      primary: '#30383d',
+      secondary: '#475259',
+      disabled: '#475259',
+      hint: '#30383d',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: 36,
+      lineHeight: 1.33,
+    },
+    h2: {
+      fontSize: 24,
+      lineHeight: 1.33,
+    },
+    h3: {
+      fontSize: 16,
+      lineHeight: 1.37,
+    },
+    caption: {
+      fontSize: 12,
+      lineHeight: 1.5,
+    },
+    body1: {
+      fontSize: 14,
+      lineHeight: 1.43,
+    },
+    body2: {
+      fontSize: 14,
     },
 });
 
