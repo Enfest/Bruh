97d9e06910534f7a47e0f88bbba2b21deae53eb5
YCK1130 <ken1130yang@gmail.com>
2024-09-08 04:55:58 +0800

    feat: addd prototype

--- web/src/components/HospitalCard.jsx
+++ web/src/components/HospitalCard.jsx
@@ -2,35 +2,157 @@
 import { useEffect, useState, useCallback } from "react";
 import { sha256 } from "js-sha256";
 //mui import
-import { Typography, TextField } from "@mui/material";
+import { Typography, TextField, Divider, Paper, Zoom, Fab, Button } from "@mui/material";
 import React from "react";
 
-import MultipleSelectChip from "./MultipleSelectChip.jsx";
-import SingleSelect from "./SingleSelect.jsx";
-import lodash from "lodash";
-import Paper from "@mui/material/Paper";
-import Zoom from "@mui/material/Zoom";
 import { useInViewport } from "../containers/hooks/isInView.js";
+import { useTheme } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+
+import Avatar from "@mui/material/Avatar";
 
 const HospitalCard = ({ id, name, clinic, avatarURL, googleMapURL, clinicURL, distance }) => {
     const key = sha256(id + name + clinic + avatarURL + googleMapURL + clinicURL + distance);
     const ref = React.useRef(null);
     const { isVisible, update } = useInViewport(ref);
+    const theme = useTheme();
+
     return (
         <Zoom
-            key={`${key}-q-wrapper`}
+            key={`${key}-wrapper`}
             in={isVisible}
             style={{ transitionDelay: isVisible ? `100ms` : "0ms" }}
             ref={ref}
         >
-            <Paper key={`${key}-background-2`} elevation={2} sx={{ padding: "10px" }}>
-                <Typography variant="subtitle1" component="div" key={`${key}-description`}>
-                    {description}
-                </Typography>
-                <div
-                    style={{ display: "flex", justifyContent: "center" }}
-                    key={`${key}-wrapper`}
-                ></div>
+            <Paper
+                key={`${key}-bg`}
+                elevation={2}
+                sx={{
+                    padding: "10px",
+                    width: "400px",
+                    height: "200px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    flexDirection: "row",
+                }}
+            >
+                <Paper
+                    key={`${key}-avatar-bg`}
+                    elevation={0}
+                    sx={{
+                        padding: "10px",
+                        width: "30%",
+                        height: "90%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Avatar
+                        key={`${key}-avatar`}
+                        alt={`${name} | ${clinic}`}
+                        src={avatarURL || "https://via.placeholder.com/150"}
+                        sx={{ height: "100px", width: "100px" }}
+                    />
+                    <Paper
+                        elevation={0}
+                        sx={{
+                            width: "100%",
+                            height: "50%",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <Typography key={`${key}-name`} variant="subtitle1" component="div">
+                            {name}
+                        </Typography>
+                        <Typography key={`${key}-clinic`} variant="subtitle1" component="div">
+                            {clinic}
+                        </Typography>
+                    </Paper>
+                </Paper>
+                <Divider orientation="vertical" flexItem />
+                {/* <div style={{ backgroundColor: "black", width: "2px", height: "100%" }}></div> */}
+                <Paper
+                    key={`${key}-card-right`}
+                    elevation={0}
+                    sx={{
+                        padding: "10px",
+                        width: "70%",
+                        height: "90%",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Paper
+                        key={`${key}-card-right-top`}
+                        elevation={0}
+                        sx={{
+                            // padding: "10px",
+                            width: "100%",
+                            height: "50%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            flexDirection: "row",
+                        }}
+                    >
+                        <Typography key={`${key}-distance`} variant="subtitle1" component="div">
+                            {`距離您：${distance}`}
+                        </Typography>
+                        <Button
+                            key={`${key}-map-butt`}
+                            variant="outlined"
+                            onClick={() => (window.location.href = googleMapURL)}
+                            sx={{
+                                ...theme?.select.MenuProps.PaperProps.style,
+                                width: "150px",
+                                height: "50px",
+                                borderRadius: "40px",
+                            }}
+                        >
+                            <Avatar
+                                key={`${key}-google-map`}
+                                alt={`${name} | ${clinic}`}
+                                src={"assets/google-maps-pin.png"}
+                                sx={{ height: "40px", width: "40px" }}
+                            ></Avatar>
+                            {"Show Map"}
+                        </Button>
+                    </Paper>
+                    <Paper
+                        key={`${key}-card-right-bottom`}
+                        elevation={0}
+                        sx={{
+                            width: "100%",
+                            height: "40%",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <Button
+                            key={`${key}-clinic-url`}
+                            variant="contained"
+                            onClick={() => (window.location.href = clinicURL)}
+                            sx={{
+                                ...theme?.select.MenuProps.PaperProps.style,
+                                width: "80%",
+                                height: "100%",
+                                borderRadius: "40px",
+                            }}
+                        >
+                            點我預約
+                        </Button>
+                    </Paper>
+                </Paper>
             </Paper>
         </Zoom>
     );
--- web/src/containers/ClinicNavigationPage.jsx
+++ web/src/containers/ClinicNavigationPage.jsx
@@ -14,6 +14,19 @@ import Question from "../components/Question.jsx";
 import CircularProgress from "@mui/material/CircularProgress";
 import Paper from "@mui/material/Paper";
 import Zoom from "@mui/material/Zoom";
+import HospitalCard from "../components/HospitalCard.jsx";
+
+const hpData = [
+    {
+        id: "1",
+        name: "name",
+        clinic: "clinic",
+        avatarURL: "https://via.placeholder.com/150",
+        googleMapURL: "https://www.google.com/maps",
+        clinicURL: "https://www.google.com",
+        distance: "1km",
+    },
+];
 
 const ClinicNavigationPage = () => {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -35,35 +48,83 @@ const ClinicNavigationPage = () => {
             }}
             key={`${key}-wrapper`}
         >
-            <Paper key={`${key}-caring-words-background`} elevation={1} sx={{ padding: "10px" }}>
+            <Paper
+                key={`${key}-caring-words-background`}
+                elevation={1}
+                sx={{
+                    padding: "10px",
+                    width: "90%",
+                    height: "300px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: dialogLoading ? "center" : "space-between",
+                    alignItems: dialogLoading ? "center" : "flex-start",
+                    justifySelf: "center",
+                }}
+            >
                 {!dialogLoading ? (
-                    <Typography variant="h5" component="div" key={`${key}-page-title`}>
-                        title
-                    </Typography>
+                    <>
+                        <Typography variant="h5" component="div" key={`${key}-page-title`}>
+                            title
+                        </Typography>
+                        <Typography variant="h3" component="div" key={`${key}-page-title`}>
+                            title
+                        </Typography>
+                    </>
                 ) : (
                     <CircularProgress />
                 )}
+                <Paper
+                    key={`${key}-caring-words-background`}
+                    elevation={0}
+                    sx={{
+                        width: "100%",
+                        height: "50px",
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        alignItems: "flex-start",
+                        justifySelf: "flex-start",
+                    }}
+                >
+                    <Typography key={`${key}-page-description`} variant="h5" component="div">
+                        description
+                    </Typography>
+                </Paper>
             </Paper>
-            <Typography
-                key={`${key}-page-description`}
-                variant="h5"
-                component="div"
-                color="text.secondary"
+            <Paper
+                key={`${key}-question-background`}
+                elevation={0}
+                sx={{
+                    padding: "10px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                    width: "90%",
+                }}
             >
-                description
-            </Typography>
+                {hpData.map((hp) => (
+                    <HospitalCard
+                        key={`${key}-hp-${hp.id}`}
+                        id={hp.id}
+                        name={hp.name}
+                        clinic={hp.clinic}
+                        avatarURL={hp.avatarURL}
+                        googleMapURL={hp.googleMapURL}
+                        clinicURL={hp.clinicURL}
+                        distance={hp.distance}
+                    />
+                ))}
+            </Paper>
             <div style={{ display: "flex", justifyContent: "center" }} key={`${key}-butt-wrapper`}>
                 {!submitted ? (
                     <Button
                         key={`${key}-next-butt`}
                         variant="contained"
-                        onClick={() =>
-                            (window.location.href =
-                                "https://webreg.tpech.gov.tw/regonline1_1_2.aspx?chaid=&tab=&deptcode=0202&deptname=%E4%B8%80%E8%88%AC%E5%85%A7%E7%A7%91")
-                        }
+                        onClick={() => (window.location.href = "https://chatgpt.com/?model=auto")}
                         sx={{ ...theme.select.MenuProps.PaperProps.style, width: 300 }}
                     >
-                        Next
+                        To Chat Bot
                     </Button>
                 ) : (
                     <Button
